a6dc122dc0a8a5dd5d9912056490769a0e7b046f
root <root@DELL10X6H24.replynet.prv>
2024-10-24 13:18:10 +0200

    modify env variables

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import mlflow.pytorch
 from mlflow import MlflowClient
 
 # Set our tracking server uri for logging
-mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
+# mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
 
 # Create a new MLflow Experiment
-mlflow.set_experiment("MNIST and Pytorch")
+# mlflow.set_experiment("MNIST and Pytorch")
 
 
 class MNISTModel(L.LightningModule):
